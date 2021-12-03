ebf258c1e2e71c8fcf7c1a6e99dbd0a6bd16db98
Vladislav Beloglazov <pro100king23@yandex.ru>
2021-12-03 15:20:34 +0300

    lesson8

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+
+    public function index(): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $reposatory = $manager->getRepository(Product::class);
+        $products = $reposatory->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+     /**
+     * @Route("/product/create", name="create_product")
+     */
+
+    public function createProduct(): RedirectResponse
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $product = new Product();
+        $product -> setTitle('Title '.random_int(0,100));
+        $product -> setPrice(random_int(1,10000));
+        $product -> setDescription('');
+        $manager->persist($product);
+        $manager->flush();
+        return $this->redirectToRoute('product');
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Price;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(float $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
